03e4cd082abbeda5649db12985a7a14bf83aa418
MoloZzz <mega_oleksii@ukr.net>
2025-06-08 00:24:29 +0300

    feat: connect postres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './common/config/env.validation';
+import { PostgresqlModule } from './libs/postgresql/postgresql.module';
+import { entities } from './common/entities';
+import { migrations } from './common/migrations';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: [`.env`],
-            validationSchema: validationSchema,
+            validationSchema,
         }),
+        PostgresqlModule.register(entities, migrations, []),
     ],
     controllers: [AppController],
     providers: [AppService],
